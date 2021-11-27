96b0e94b26874190c25c0f2388e12d5b34f72ffb
sms <shatild12@gmail.com>
2021-11-27 20:50:57 +0600

    4rth

--- bin/d_basic.dart
+++ bin/d_basic.dart
@@ -1,6 +1,26 @@
 void main() {
-  var useName = 'Sarwar Murshed Shatil';
-  var userId = 12345;
+  // var useName = 'Sarwar Murshed Shatil';
+  // var userId = 12345;
+  //
+  // print('Username : $useName UserId: $userId');
+}
 
-  print('Username : $useName UserId: $userId');
+void myMethodName(//Argument){
+//body
+
+}
+
+int myId(//Argument){
+  //body
+return 0;
+}
+
+String myName(//Argument){
+  //body
+return 0;
+}
+
+double myCGP(//Argument){
+  //body
+return 0;
 }
